4db39500994d7ef454ac74b1b1d85cc4c7467be1
IdeaSeeker <stroganov.n.36@gmail.com>
2022-11-30 16:34:12 +0300

    add hw2

--- homework-02/main/main.go
+++ homework-02/main/main.go
@@ -1,19 +1,50 @@
 package main
 
 import (
-//    "fmt"
-    "dqueue"
-//    "context"
-
-    "github.com/go-redis/redis/v8"
+	"fmt"
+	"strconv"
+	"github.com/go-redis/redis/v8"
+	"dqueue"
+	"os"
 )
 
+// 158.160.9.8:6379 51.250.106.140:6379 158.160.19.212:6379 158.160.19.2:6379 158.160.9.8 51.250.106.140 158.160.19.212
 func main() {
-    redisOptions := redis.Options{
-        Addr:     "localhost:6379",
-        Password: "", // no password set
-        DB:       0,  // use default DB
-    }
+	hosts := os.Args[1:]
+	if len(hosts) != 7 {
+		fmt.Println("Expected 4 addresses of redis shards and 3 addresses of zookeeper cluster")
+		return
+	}
 
-    dqueue.Config(&redisOptions, []string{"127.0.0.1"})
+	var redisOptions = []*redis.Options{
+		{ Addr: hosts[0], Password: "", DB: 0 },
+		{ Addr: hosts[1], Password: "", DB: 0 },
+		{ Addr: hosts[2], Password: "", DB: 0 },
+		{ Addr: hosts[3], Password: "", DB: 0 },
+	}
+	var zkCluster = []string{ hosts[4], hosts[5], hosts[6] }
+
+	dqueue.Config(&redisOptions, zkCluster)
+	q, err := dqueue.Open("nikita:queue2", 4)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		err := q.Push(strconv.Itoa(i))
+		if err != nil {
+			panic(err)
+		}
+	}
+	println("Pushed 10 values")
+
+	println("Poll:")
+	for i := 0; i < 10; i++ {
+		value, err := q.Pull()
+		if err != nil {
+			panic(err)
+		} else {
+			println(value)
+		}
+	}
 }
