7dbc6e72fda76c48e17c3ea170a3c37f82453951
IdeaSeeker <stroganov.n.36@gmail.com>
2022-12-29 00:32:47 +0300

    fix spaces

--- homework-03/redis.go
+++ homework-03/redis.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"context"
+    "context"
 
-	"github.com/go-redis/redis/v8"
+    "github.com/go-redis/redis/v8"
 )
 
 var cnxt = context.Background()
@@ -11,15 +11,15 @@ var redisOptions = redis.Options{Addr: REDIS_HOST, Password: "", DB: 0}
 var redisClient *redis.Client = redis.NewClient(&redisOptions)
 
 func RedisSetUrls(tinyurl string, longurl string) error {
-	tinyurlKey := constructRedisKey(tinyurl)
-	return redisClient.Set(cnxt, tinyurlKey, longurl, 0).Err()
+    tinyurlKey := constructRedisKey(tinyurl)
+    return redisClient.Set(cnxt, tinyurlKey, longurl, 0).Err()
 }
 
 func RedisGetLongurl(tinyurl string) (string, error) {
-	tinyurlKey := constructRedisKey(tinyurl)
-	return redisClient.Get(cnxt, tinyurlKey).Result()
+    tinyurlKey := constructRedisKey(tinyurl)
+    return redisClient.Get(cnxt, tinyurlKey).Result()
 }
 
 func constructRedisKey(tinyurl string) string {
-	return USERNAME + "|" + tinyurl
+    return USERNAME + "|" + tinyurl
 }
