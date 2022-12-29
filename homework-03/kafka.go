fd3dddd99f1aab46748f252386f8035b3bbe1fa3
IdeaSeeker <stroganov.n.36@gmail.com>
2022-12-29 03:36:19 +0300

    fix spaces

--- homework-03/kafka.go
+++ homework-03/kafka.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"context"
-	"fmt"
+    "context"
+    "fmt"
 
-	"github.com/segmentio/kafka-go"
+    "github.com/segmentio/kafka-go"
 )
 
 const (
     KAFKA_URL_TOPIC_NAME = USERNAME + "-url-topic"
-	KAFKA_CLICKS_TOPIC_NAME = USERNAME + "-clicks-topic"
+    KAFKA_CLICKS_TOPIC_NAME = USERNAME + "-clicks-topic"
 )
 
 var cntx = context.Background()
@@ -23,93 +23,93 @@ var kafkaClicksProducer *kafka.Writer = initKafkaProducer(KAFKA_CLICKS_TOPIC_NAM
 // urls
 
 func KafkaPushUrls(tinyurl string, longurl string) {
-	err := kafkaUrlProducer.WriteMessages(cntx,
-		kafka.Message{
-			Key:   []byte(tinyurl),
-			Value: []byte(longurl),
-		},
-	)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("kafkaUrlProducer: pushed {" + tinyurl + ", " + longurl + "}")
+    err := kafkaUrlProducer.WriteMessages(cntx,
+        kafka.Message{
+            Key:   []byte(tinyurl),
+            Value: []byte(longurl),
+        },
+    )
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("kafkaUrlProducer: pushed {" + tinyurl + ", " + longurl + "}")
 }
 
 func KafkaRunUrlConsumer(groupId string) {
-	kafkaUrlConsumer = initKafkaConsumer(KAFKA_URL_TOPIC_NAME, groupId)
-	for {
-		message, err := kafkaUrlConsumer.FetchMessage(cntx)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+    kafkaUrlConsumer = initKafkaConsumer(KAFKA_URL_TOPIC_NAME, groupId)
+    for {
+        message, err := kafkaUrlConsumer.FetchMessage(cntx)
+        if err != nil {
+            fmt.Println(err)
+            continue
+        }
 
-		tinyurl, longurl := string(message.Key), string(message.Value)
-		fmt.Println("KafkaUrlConsumer: read {" + tinyurl + ", " + longurl + "}")
+        tinyurl, longurl := string(message.Key), string(message.Value)
+        fmt.Println("KafkaUrlConsumer: read {" + tinyurl + ", " + longurl + "}")
 
-		err = RedisSetUrls(tinyurl, longurl)
-		if err != nil {
-			panic(err)
-		}
+        err = RedisSetUrls(tinyurl, longurl)
+        if err != nil {
+            panic(err)
+        }
         fmt.Println("Redis: set " + tinyurl + " -> " + longurl)
 
-		err = kafkaUrlConsumer.CommitMessages(cntx, message)
-		if err != nil {
-			panic(err)
-		}
-	}
+        err = kafkaUrlConsumer.CommitMessages(cntx, message)
+        if err != nil {
+            panic(err)
+        }
+    }
 }
 
 // clicks
 
 func KafkaPushClicks(tinyurl string, plusClicksNumber int64) {
     clicksString := fmt.Sprint(plusClicksNumber)
-	err := kafkaClicksProducer.WriteMessages(cntx,
-		kafka.Message{
-			Key:   []byte(tinyurl),
-			Value: []byte(clicksString),
-		},
-	)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("kafkaClicksProducer: pushed {" + tinyurl + ", " + clicksString + "}")
+    err := kafkaClicksProducer.WriteMessages(cntx,
+        kafka.Message{
+            Key:   []byte(tinyurl),
+            Value: []byte(clicksString),
+        },
+    )
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("kafkaClicksProducer: pushed {" + tinyurl + ", " + clicksString + "}")
 }
 
 func KafkaRunClicksConsumer(groupId string) {
-	kafkaClicksConsumer = initKafkaConsumer(KAFKA_CLICKS_TOPIC_NAME, groupId)
-	for {
-		message, err := kafkaClicksConsumer.FetchMessage(cntx)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+    kafkaClicksConsumer = initKafkaConsumer(KAFKA_CLICKS_TOPIC_NAME, groupId)
+    for {
+        message, err := kafkaClicksConsumer.FetchMessage(cntx)
+        if err != nil {
+            fmt.Println(err)
+            continue
+        }
 
-		tinyurl, plusClicksNumber := string(message.Key), string(message.Value)
-		fmt.Println("kafkaClicksConsumer: read {" + tinyurl + ", " + plusClicksNumber + "}")
+        tinyurl, plusClicksNumber := string(message.Key), string(message.Value)
+        fmt.Println("kafkaClicksConsumer: read {" + tinyurl + ", " + plusClicksNumber + "}")
 
         err = SqlUpdateClicks(tinyurl, plusClicksNumber)
-		if err != nil {
-			panic(err)
-		}
+        if err != nil {
+            panic(err)
+        }
         fmt.Println("Sql: updated clicks on " + tinyurl + " by " + plusClicksNumber)
 
-		err = kafkaClicksConsumer.CommitMessages(cntx, message)
-		if err != nil {
-			panic(err)
-		}
-	}
+        err = kafkaClicksConsumer.CommitMessages(cntx, message)
+        if err != nil {
+            panic(err)
+        }
+    }
 }
 
 // inits
 
 func initKafkaConsumer(topic string, groupId string) *kafka.Reader {
-	return kafka.NewReader(kafka.ReaderConfig{
-		Brokers:   []string{KAFKA_HOST},
-		GroupID:   groupId,
-		Topic:     topic,
-		Partition: 0,
-	})
+    return kafka.NewReader(kafka.ReaderConfig{
+        Brokers:   []string{KAFKA_HOST},
+        GroupID:   groupId,
+        Topic:     topic,
+        Partition: 0,
+    })
 }
 
 func initKafkaProducer(topic string) *kafka.Writer {
--- homework-03/util.go
+++ homework-03/util.go
@@ -5,21 +5,21 @@ import "strings"
 const abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func LongurlIdToTinyurl(longurlId int64) string {
-	tinyurl := ""
-	for longurlId > 0 {
-		tinyurl = string(abc[longurlId%int64(len(abc))]) + tinyurl
-		longurlId /= int64(len(abc))
-	}
-	for len(tinyurl) < 7 {
-		tinyurl = string(abc[0]) + tinyurl
-	}
-	return tinyurl
+    tinyurl := ""
+    for longurlId > 0 {
+        tinyurl = string(abc[longurlId%int64(len(abc))]) + tinyurl
+        longurlId /= int64(len(abc))
+    }
+    for len(tinyurl) < 7 {
+        tinyurl = string(abc[0]) + tinyurl
+    }
+    return tinyurl
 }
 
 func TinyurlToLongurlId(tinyurl string) int64 {
-	longurlId := int64(0)
-	for _, c := range tinyurl {
-		longurlId = longurlId*int64(len(abc)) + int64(strings.IndexRune(abc, c))
-	}
-	return longurlId
+    longurlId := int64(0)
+    for _, c := range tinyurl {
+        longurlId = longurlId*int64(len(abc)) + int64(strings.IndexRune(abc, c))
+    }
+    return longurlId
 }
